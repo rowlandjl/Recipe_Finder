a37f3a22e64cbd47e9bb8803e4dcab21fefc63d2
rowlandjl <rowlandjl@vcu.edu>
2016-07-01 22:41:24 -0400

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :user_allergies
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
