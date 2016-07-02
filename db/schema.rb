b88f6ec0306acf46bd1ec7bd6f2540e3067993ca
rowlandjl <rowlandjl@vcu.edu>
2016-07-01 22:53:16 -0400

    migrate diet table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702022751) do
+ActiveRecord::Schema.define(version: 20160702024306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "allergies", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "diets", force: :cascade do |t|
     t.string "name"
   end
 
