c8b61bf346bbee083fdfca1b9cb1262ccb21c178
rowlandjl <rowlandjl@vcu.edu>
2016-07-01 23:01:25 -0400

    migrate user_diets join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702024306) do
+ActiveRecord::Schema.define(version: 20160702025525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20160702024306) do
   create_table "user_allergies", force: :cascade do |t|
     t.integer "user_id",    null: false
     t.integer "allergy_id", null: false
+  end
+
+  create_table "user_diets", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "diet_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
